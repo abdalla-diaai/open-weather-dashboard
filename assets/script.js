105c2ff716f12edf97aa4ce447653f85d72f660a
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-01-26 19:49:09 +0000

    added cards for display.

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,56 @@
 var cityHistory = [];
+var todayDiv = $('#today');
+var forecastDiv = $('#forecast');
 var city;
 var temp;
 var wind;
 var humidity;
+var weatherDiv;
+var weatherBody;
+var weatherInfo;
 var weatherUl;
 var tempLi;
 var windLi;
 var humidityLi;
 var today;
+var forecastDay;
 
+// function to create cards for the specific day weather
+function createCard(date, dateFormat, wDiv) {
+    weatherDiv = $('<div>');
+    wDiv.append(weatherDiv);
+    weatherInfo = $('<h5>');
+    if (date === 'today') {
+        weatherDiv.attr('class', 'card');
+        today = dateFormat;
+        weatherInfo.text(`${city} ${today}`);
+    };
+    if (date === 'forecast') {
+        weatherDiv.attr('class', 'card col');
+        forecastDay = dateFormat;
+        weatherInfo.text(forecastDay.toLocaleDateString());
+    };
+    weatherInfo.attr('class', 'card-title');
+    weatherDiv.append(weatherInfo);
+    weatherBody = $('<div>');
+    weatherBody.attr('class', 'card-body');
+    weatherDiv.append(weatherBody);
+    weatherUl = $('<ul>');
+    weatherUl.attr('class', 'card-text');
+    tempLi = $('<li>');
+    windLi = $('<li>');
+    humidityLi = $('<li>');
+    weatherUl.append(tempLi);
+    weatherUl.append(windLi);
+    weatherUl.append(humidityLi);
+    weatherBody.append(weatherUl);
 
+    tempLi.text(`Temperature: ${temp}ºC`);
+    windLi.text(`Wind Speed: ${wind} mps`);
+    humidityLi.text(`Humidity: ${humidity}%`);
+};
 
-$('#search-form').on('submit', function (event) {
-    event.preventDefault();
-    today = dayjs().format('DD-MM-YYYY');
-    city = $('#search-input').val();
+function processForm(city) {
     $('#history').prepend($('<button>').text(city));
     var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=ba14af29e70b969c97f97a7190344c26`
 
@@ -24,41 +60,27 @@ $('#search-form').on('submit', function (event) {
         })
         .then(function (data) {
             // YOUR CODE GOES HERE!!!
+            forecast = dayjs().add(1, 'day');
             temp = JSON.stringify(data.list[0].main.temp);
             wind = JSON.stringify(data.list[0].wind.speed);
             humidity = JSON.stringify(data.list[0].main.humidity);
-            weatherUl = $('<ul>');
-            tempLi = $('<li>');
-            windLi = $('<li>');
-            humidityLi = $('<li>');
-            weatherUl.append(tempLi);
-            weatherUl.append(windLi);
-            weatherUl.append(humidityLi);
+            createCard('today', dayjs().format('DD-MM-YYYY'), todayDiv);
 
-            tempLi.text(`Temperature: ${temp}ºC`);
-            windLi.text(`Wind Speed: ${wind} mps`);
-            humidityLi.text(`Humidity: ${humidity}%`);
-            $('#today').append(weatherUl);
-            console.log(data.list)
             for (var i = 8; i < data.list.length; i += 8) {
-                $('<div>')
                 temp = JSON.stringify(data.list[i].main.temp);
                 wind = JSON.stringify(data.list[i].wind.speed);
                 humidity = JSON.stringify(data.list[i].main.humidity);
-                weatherUl = $('<ul>');
-                tempLi = $('<li>');
-                windLi = $('<li>');
-                humidityLi = $('<li>');
-                weatherUl.append(tempLi);
-                weatherUl.append(windLi);
-                weatherUl.append(humidityLi);
-
-                tempLi.text(`Temperature: ${temp}ºC`);
-                windLi.text(`Wind Speed: ${wind} mps`);
-                humidityLi.text(`Humidity: ${humidity}%`);
-                $('#forecast').append(weatherUl);
+                
+                createCard('forecast', new Date(data.list[i].dt_txt), forecastDiv);
             }
-
         });
     $('#search-input').val('');
-})  
+};
+$('#search-form').on('submit', function (event) {
+    todayDiv.empty();
+    forecastDiv.empty();
+    event.preventDefault();
+    city = $('#search-input').val();
+    cityHistory.push(city);
+    processForm(city);
+});
